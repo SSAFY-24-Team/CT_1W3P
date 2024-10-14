f91a6a1c6a5156f4fdd4a7a07d89bea19843168d
강수민 <kangsm423@gmail.com>
2024-10-14 15:55:33 +0900

    7주차_강수민

---
+++ 7주차/Boj_무한이진트리_강수민.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int left = 0;
+        int right = 0;
+
+        while (A > 1 || B > 1) {
+            if (A > B) {
+                left++;
+                A -= B;
+            } else {
+                right++;
+                B -= A;
+            }
+        }
+
+        sb.append(left).append(' ').append(right);
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
